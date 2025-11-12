da61e641f24a55ebf7e73e4c7558d9b1a3c1b43c
Gourav Sapaliga <acd07514@gmail.com>
2025-11-12 07:27:43 +0530

    Add C++ build task and implement min/max functions in PointersLesson4

--- PointersLesson4.cpp
+++ PointersLesson4.cpp
@@ -1,8 +1,48 @@
-#include<stdio.h>
+#include <iostream>
 using namespace std;
 
-int main(){
+int getMin(int numbers[], int sizeOfArray)
+{
+    int min = numbers[0];
+    for (int i = 0; i < sizeOfArray; i++)
+    {
+        if (numbers[i] < min)
+            min = numbers[i];
+    }
+    return min;
+}
+int getMax(int numbers[], int sizeOfArray)
+{
+    int max = numbers[0];
+    for (int i = 0; i < sizeOfArray; i++)
+    {
+        if (numbers[i] > max)
+            max = numbers[i];
+    }
+    return max;
+}
+
+int getMinAndMax(int numbers[], int sizeOfTheArray, int *max, int *min)
+{
+    for (int i = 0; i < sizeOfTheArray; i++)
+    {
+        if (numbers[i] > *max)
+            *max = numbers[i];
+        if (numbers[i] < *min)
+            *min = numbers[i];
+    }
+}
+int main()
+{
     int numbers[5] = {5, -4, 2, 8, 20};
-    
+
+    int max = numbers[0];
+    int min = numbers[0];
+
+    getMinAndMax(numbers, 5, &max, &min);
+    cout << "The maximum is --> " << max << endl;
+    cout << "The minimum is --> " << min << endl;
+    // cout << "The maximum is --> " << getMax(numbers, 5) << endl;
+    // cout << "The minimum is --> " << getMin(numbers, 5) << endl;
     return 0;
 }
