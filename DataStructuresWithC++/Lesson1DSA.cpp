6ae41849a5d29ab00cdd5fa87df018cfc6fcd2a4
Gourav Sapaliga <130655508+shadowofdominance@users.noreply.github.com>
2025-12-31 11:34:00 +0530

    Revised Lesson 1

--- DataStructuresWithC++/Lesson1DSA.cpp
+++ DataStructuresWithC++/Lesson1DSA.cpp
@@ -5,12 +5,20 @@ int main()
 {
     // Initializing an array!
     float monthsArray[6];
+    float total = 0;
+    float average = 0; 
 
     for (int i = 0; i < 6; i++)
     {
         cout << "Enter the amount saved in month " << i + 1 << ": ";
         cin >> monthsArray[i];
+        total += monthsArray[i];
     }
+
+    average = total / 6;
+
+    cout << "The total savings are: " << total << endl;
+    cout << "The average savings are: " << average << endl;
 
     return 0;
 }   
