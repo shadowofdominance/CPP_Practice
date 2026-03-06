8a2a11e1c52d6b3e6547bb52c45a50dcf4d2dcd2
Gourav Sapaliga <130655508+shadowofdominance@users.noreply.github.com>
2026-03-06 19:42:21 +0530

    Practiced header and source files

---
+++ Header_Source/Food.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "Food.h"
+
+const char* RecommendMeAFood(char firstLetter) {
+	if (firstLetter == 'A' || firstLetter == 'a')
+		return "apple";
+	else if (firstLetter == 'B' || firstLetter == 'b')
+		return "Banana";
+	else if (firstLetter == 'C' || firstLetter == 'c')
+		return "Cake";
+	else
+		return "pizza";
+}
+
+void GetPizzaRecipe() {
+	std::cout << "Here is the pizza recipe...."
+}
---
+++ Header_Source/Header_Source.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Food.h"
+
+int main()
+{
+	int age;
+	age = 22;
+	std::cout << "Hello World!\n";
+	std::cout << age;
+	std::cout << "Today I will be eating " << RecommendMeAFood('c');
+	std::cout << "Pizza recipe is: " << GetPizzaRecipe;
+}
